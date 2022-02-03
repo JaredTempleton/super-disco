c8cef313fa95ef5e93607fbf82d656263d391336
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-02-02 23:54:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-var todaysDate = moment().format("dddd, MMM Do YYYY");
-$("#currentDay").html(todaysDate);
+var todayDate = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(todayDate);
 
-$(document).ready(function () {
-    $(".savebtn").on("click", function () {
+$(document).ready(function () { 
+    $(".saveBtn").on("click", function () {
         var text = $(this).siblings(".description").val();
         var time = $(this).parent().attr("id");
 
         localStorage.setItem(time, text);
     })
-
+   
     function timeTracker() {
         var timeNow = moment().hour();
 
@@ -29,19 +29,21 @@ $(document).ready(function () {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
+
             }
         })
     }
 
-    $("hour9 .description").val(localStorage.getItem("hour9"));
-    $("hour10 .description").val(localStorage.getItem("hour10"));
-    $("hour11 .description").val(localStorage.getItem("hour11"));
-    $("hour12 .description").val(localStorage.getItem("hour12"));
-    $("hour13 .description").val(localStorage.getItem("hour13"));
-    $("hour14 .description").val(localStorage.getItem("hour14"));
-    $("hour15 .description").val(localStorage.getItem("hour15"));
-    $("hour16 .description").val(localStorage.getItem("hour16"));
-    $("hour17 .description").val(localStorage.getItem("hour17"));
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
     timeTracker();
 })
